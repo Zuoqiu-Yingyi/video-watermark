53125affc9e2c2886d48f48fa4acc5e65dd5181f
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2022-12-04 00:36:18 +0800

    feat: 新增 `直方图均衡攻击`, `帧重复攻击`, `帧插入攻击`, `帧交换攻击` 四种攻击方式

--- pkg/attack.py
+++ pkg/attack.py
@@ -1,4 +1,6 @@
 # -*-coding: UTF-8 -*-
+
+
 import typing as T
 
 import numpy as np
@@ -10,18 +12,16 @@ def none(src: np.array) -> np.array:
     return src.copy()
 
 
-# 帧删除攻击
-def frameDelete(src: np.array, count: int = 0) -> np.array:
-    return src[..., count:].copy()
-
-
-# 缩放攻击
-def scale(src: np.array, rate: float) -> np.array:
+# 裁剪攻击(保留的内容平移到左上角)
+def cut(src: np.array, height: None | slice = None, width: None | slice = None) -> np.array:
     rst = src.copy()
-    height, width, _ = src.shape
-    dsize = (width, height)
-    dsize_scale = (int(width * rate), int(height * rate))
-    rst = cv.resize(cv.resize(src, dsize=dsize_scale), dsize=dsize)
+    if height is None:
+        height = slice(src.shape[0])
+    if width is None:
+        width = slice(src.shape[1])
+    rst_height = len(range(*height.indices(src.shape[0])))
+    rst_width = len(range(*width.indices(src.shape[1])))
+    rst[:rst_height, :rst_width, ...] = rst[height, width, ...]
     return rst
 
 
@@ -49,8 +49,18 @@ def rotate(src: np.array, angle: float, recover: bool = True) -> np.array:
         return cv.warpAffine(src, M1, dsize=dsize_stage)
 
 
-# 裁剪攻击
-def cut(src: np.array, slices: T.List[T.Tuple[slice, slice]] = []) -> np.array:
+# 缩放攻击
+def scale(src: np.array, rate: float) -> np.array:
+    rst = src.copy()
+    height, width, _ = src.shape
+    dsize = (width, height)
+    dsize_scale = (int(width * rate), int(height * rate))
+    rst = cv.resize(cv.resize(src, dsize=dsize_scale), dsize=dsize)
+    return rst
+
+
+# 遮盖攻击
+def mark(src: np.array, slices: T.List[T.Tuple[slice, slice]] = []) -> np.array:
     rst = src.copy()
     for s in slices:
         rst[s[0], s[1], ...] = 0
@@ -65,3 +75,51 @@ def noise(src: np.array, handler: T.Callable[[np.array], np.array], *args, **kw)
 # 滤波攻击
 def filte(src: np.array, handler: T.Callable[[np.array], np.array], *args, **kw) -> np.array:
     return handler(src, *args, **kw)
+
+
+# 直方图均衡
+def equalizeHist(src: np.array) -> np.array:
+    rst = src.copy()
+    for i in range(src.shape[2]):
+        rst[..., i] = cv.equalizeHist(src[..., i])
+    return rst
+
+
+# 帧重复攻击(重复帧覆盖原帧, count 为一帧重复次数)
+def frameRepeat(src: np.array, count: int = 0) -> np.array:
+    count += 1
+    rst = src.copy()
+    for i in range(src.shape[2]):
+        if i % count == 0:
+            frame = src[..., i]
+        else:
+            rst[..., i] = frame
+    return rst
+
+
+# 帧删除攻击
+def frameDelete(src: np.array, count: int = 0) -> np.array:
+    return src[..., count:].copy()
+
+
+# 帧插入攻击(重复帧作为新的帧插入)
+def frameInsert(src: np.array, count: int = 0) -> np.array:
+    count += 1
+    j = 0
+    rst = src.copy()
+    for i in range(src.shape[2]):
+        if i % count == 0:
+            frame = src[..., j]
+            j += 1
+        else:
+            rst[..., i] = frame
+    return rst
+
+
+# 帧交换攻击
+def frameSwap(src: np.array) -> np.array:
+    rst = src.copy()
+    for i in range(src.shape[2] // 2):
+        rst[..., i] = src[..., i+1]
+        rst[..., i+1] = src[..., i]
+    return rst
